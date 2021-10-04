86605f7be13d894ce440cdf6d5e4663f2bdf6294
qhahd78 <umuj0708@likelion.org>
2021-10-04 19:37:41 +0900

    Setting :: graphql setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./App.css";
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo/client';
+import Main from './components/Main';
 
 function App() {
   return (
-    <>
-      <h1>hello minseoks</h1>
-    </>
+    // eslint-disable-next-line react/jsx-filename-extension
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
   );
 }
 
