556fd19ff1c79d4c8fe967f93d73065d8985aca4
qhahd78 <umuj0708@likelion.org>
2021-10-04 20:14:06 +0900

    Setting :: Redux setting

---
+++ src/redux/action/main.js
@@ -0,0 +1,11 @@
+// redux action
+// export const addCat = () => ({
+//   type: 'ADD_CAT',
+//   payload,
+// });
+
+// const ADD_CAT = 'ADD_CAT';
+// export const addCatAction = () => ({
+//   type: ADD_CAT,
+//   payload,
+// });
