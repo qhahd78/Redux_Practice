4164cb9060da1bc058f4fcc312cb0f3e14c6e48e
qhahd78 <umuj0708@likelion.org>
2022-01-27 22:58:41 +0900

    [Fix]::graphQL del

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,38 +1,9 @@
 import React from 'react';
-import { gql } from 'apollo-boost';
-import { Query } from 'react-apollo';
-
-const GET_CATS = gql`
-  query {
-    cats {
-      id
-      name
-      age
-      face
-    }
-  }
-`;
 
 function Main() {
   return (
     <div>
-      <Query query={GET_CATS}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading . . .</p>;
-          if (error) return <p>Error !</p>;
-          return (
-            <p>
-              {data.cats.map(({ id, name, age, face }) => (
-                <>
-                  <p key={id}>
-                    이름: {name} 나이: {age} 특징: {face}
-                  </p>
-                </>
-              ))}
-            </p>
-          );
-        }}
-      </Query>
+      메인
     </div>
   );
 }
